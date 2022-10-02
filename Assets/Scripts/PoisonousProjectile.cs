cb9a2833855e10fd812c02b911a06318e488ef07
FranzRome <francesco.romeo-work@libero.it>
2022-10-03 01:13:20 +0200

    Refactor
    Renamed projectyle types and their felds

---
+++ Assets/Scripts/PoisonousProjectile.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ProjectilePoisonous : Projectile
+{
+
+   public float poisonDuration, poisonEffectFrequency, poisonDamage;
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Projectile : MonoBehaviour
+{
+
+   public float speed, damage, lifeTime;
+   public Transform target;
+
+   void Start()
+   {
+      Destroy(gameObject, lifeTime);
+   }
+
+   // Update is called once per frame
+   void Update()
+   {
+      transform.LookAt(target);
+      transform.Translate(Vector3.forward * speed * Time.deltaTime);
+   }
+
+   void OnTriggerEnter(Collider other)
+   {
+      if (other.tag == "Enemy")
+      {
+         Destroy(gameObject);
+      }
+   }
+}
