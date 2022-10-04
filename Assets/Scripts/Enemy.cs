6dc279b8b2e03af2fdfc3caf2d2aacfdabb2baed
Franz <francesco.romeo-work@libero.it>
2022-10-04 17:44:10 +0200

    Fields Tide Up

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -9,15 +9,18 @@ public class Enemy : MonoBehaviour
    public int maxHp = 100;
    protected int hp;
    public int damage = 5;
-   public Text damageText;
+
    public int pathNumber;
    private Path path;
    private int pathPoint; //punto del percorso a cui sta puntando
-   public Image hpBar;
-   public int droppedResources = 20;  //risorse che vengono guadagnate all'uccisione
    public float attackRange = 1.0f;
    public float attackDelay = 3.0f; //il tempo che impiega per attaccare
    private float nextAttackDelay;  //contatore del tempo per il prossimo attacco
+   public int droppedResources = 20;  //risorse che vengono guadagnate all'uccisione
+
+   // UI elements
+   public Text damageText;
+   public Image hpBar;
 
    private GameManager gameManager;
    private Animator animator;
