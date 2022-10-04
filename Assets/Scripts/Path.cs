6451e35f22628e8b3acae8a46b82bfe6a258ff92
Franz <francesco.romeo-work@libero.it>
2022-10-04 12:50:29 +0200

    Path automation
    Automated path waypoints picking at the start of the game

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -1,6 +1,19 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
 
 public class Path : MonoBehaviour
 {
    public Transform[] waypoints;
+
+   void Start()
+   {
+      List<Transform> waypointsList = new List<Transform>();
+
+      for (int i = 0; i < transform.childCount; i++)
+      {
+         waypointsList.Add(transform.GetChild(i));
+      }
+
+      waypoints = waypointsList.ToArray();
+   }
 }
