d41cc55544a32e514aaf6c1de6b85fe79a2152df
Franz <francesco.romeo-work@libero.it>
2022-10-06 17:23:46 +0200

    File Rename

---
+++ Assets/Scripts/DamageText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DamageText : MonoBehaviour
+{
+
+   public float tempo, velocita;
+
+   // Update is called once per frame
+   void Update()
+   {
+      tempo -= Time.deltaTime;
+      gameObject.transform.Translate(0, velocita * Time.deltaTime, 0);
+      if (tempo <= 0)
+         Destroy(gameObject);
+   }
+}
