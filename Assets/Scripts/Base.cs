acef2b9f3079b2aed7d8aa74a04d4c3d61777124
Franz <francesco.romeo-work@libero.it>
2022-11-03 19:36:21 +0100

    Added UI calculations

--- Assets/Scripts/Base.cs
+++ Assets/Scripts/Base.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class Base : MonoBehaviour
 {
    public int hp;
+   private int maxHp;
+   
+   public RectTransform hpIndicator;
+   public Sprite intactLightbulb;
+   public Sprite brokenLightbulb;
+   
+   private Image[] bulbs;
    private GameManager gameManager;
 
 
    void Start()
    {
+      maxHp = hp;
+      
       gameManager = Camera.main.GetComponent<GameManager>();
+      
+      bulbs = new Image[hpIndicator.childCount];
+
+      for (int i = 0; i < bulbs.Length; i++)
+      {
+         bulbs[i] = hpIndicator.GetChild(i).GetComponent<Image>();
+      }
    }
 
    public void TakeDamage(int damage)
@@ -21,5 +38,23 @@ public class Base : MonoBehaviour
    {
       if (hp <= 0)
          gameManager.OnGameOver();
+   }
+
+   private void UpdateHpUI()
+   {
+      int count = hpIndicator.childCount;
+      int intactLightbulbs = count / (maxHp * hp);
+
+      for (int i = 0; i < count; i++)
+      {
+         if (i < intactLightbulbs)
+         {
+            bulbs[i].sprite = intactLightbulb;
+         }
+         else
+         {
+            bulbs[i].sprite = brokenLightbulb;
+         }
+      }
    }
 }
