4a66191cb48783114870afbca5c3a7dc741d5b83
FranzRome <francesco.romeo-work@libero.it>
2022-11-15 03:26:16 +0100

    Update AreaDamageTurret.cs

--- Assets/Scripts/AreaDamageTurret.cs
+++ Assets/Scripts/AreaDamageTurret.cs
@@ -4,5 +4,15 @@ using UnityEngine;
 
 public class AreaDamageTurret : Turret
 {
-    
+    protected override void FindEnemies()
+    {
+        int maxColliders = 10;
+        Collider[] hitColliders = new Collider[maxColliders];
+        int numColliders = Physics.OverlapSphereNonAlloc(transform.position, attackRange, hitColliders, enemiesMask);
+
+        for (int i = 0; i < numColliders; i++)
+        {
+            hitColliders[i].GetComponent<Enemy>().TakeDamage(damage);
+        }
+    }
 }
