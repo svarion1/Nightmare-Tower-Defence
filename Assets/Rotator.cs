b0d5da326a7349a980513bb2a8523046414ebfbf
Restyled.io <commits@restyled.io>
2022-11-16 19:59:39 +0000

    Restyled by clang-format

--- Assets/Rotator.cs
+++ Assets/Rotator.cs
@@ -2,13 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Rotator : MonoBehaviour
-{
+public class Rotator : MonoBehaviour {
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime);
-
-    }
+  // Update is called once per frame
+  void Update() { transform.Rotate(new Vector3(0, 50, 0) * Time.deltaTime); }
 }
